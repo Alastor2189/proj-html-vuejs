75af7f92b76ab9032fe45e5f2865a563f5d7bee8
Alastor2189 <g.lucafarci89@gmail.com>
2022-06-01 01:54:45 +0200

    strutturazione header ed aggiunta immagini nei components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,55 @@
 <template>
   <div id="app">
-  
+    <AppHeader :headerMenu="headerMenu" :rightHeader="rightHeader" />
+    <AppMain />
+    <AppFooter />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 
 export default {
   name: 'App',
   components: {
-  }
-}
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+  data: function () {
+    return {
+      headerMenu : [{
+        text: "Home"
+      },
+      {
+        text: "Pages"
+      },
+      {
+        text: "Courses"
+      },
+      {
+        text: "Features"
+      },
+      {
+        text: "Blog"
+      },
+      {
+        text: "Shop"
+      },
+      ],
+
+      rightHeader :[
+        {
+          icon: "fas fa-shopping-cart"
+        }
+      ]
+    };
+  },
+};
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,39 @@
+<template>
+    <header class="header">
+        <div class="left-header">
+            <nav>
+                <ul>
+                    <li v-for="(item, index) in headerMenu" :key="index"> <h3> {{ item.text }}</h3></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="center-header">
+            <img class="logo" src="" alt="">
+        </div>
+        <div class="right-header">
+            <ul>
+                <li v-for="(item, index) in rightHeader" :key="index">
+                <i :class="item.icon"></i></li>
+            </ul>
+        </div>
+
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: 'AppHeader',
+    props: {
+        headerMenu: Array,
+        rightHeader: Array, 
+
+    },
+    components: {
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@fortawesome/fontawesome-free/css/all.min.css";
+</style>
